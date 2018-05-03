2788b18e29adc2c5b1475da2c50366f906d380ce
dweckerly <d.weckerly@gmail.com>
2018-05-03 10:47:34 -0500

    adding new components

--- scripts/paths.js
+++ scripts/paths.js
@@ -2,3 +2,6 @@ var mapComp = "components/map/map.html";
 var gHeaderComp = "components/gameHeader/gameHeader.html";
 var locComp = "components/location/location.html";
 var navComp = "components/nav/nav.html";
+var partyComp = "components/party/party.html";
+var cSheetComp = "components/charSheet/charSheet.html";
+var invComp = "components/inventory/inventory.html";
