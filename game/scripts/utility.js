f505ec56964f8533ee0f1844f8ee4b64a8145129
David <d.weckerly@gmail.com>
2018-06-18 19:51:03 -0500

    moved the game into separate folder

---
+++ game/scripts/utility.js
@@ -0,0 +1,45 @@
+$(window).on('resize', function() {
+    var win = $(this);
+    if (win.width() > 600) {
+        $('#nav-div').width(250);
+    }
+    if (win.width() <= 600) {
+        $('#nav-div').width(0);
+    }
+});
+
+function insertHTML(id, path, callback) {
+    $.get(path, function(data) {
+        $(id).html(data).hide().fadeIn('fast', function() {});
+        if (callback != undefined) {
+            callback();
+        }
+    });
+
+}
+
+function appendHTML(id, path, callback) {
+    $.get(path, function(data) {
+        $(id).append(data).hide().fadeIn('fast', function() {});
+        if (callback != undefined) {
+            callback();
+        }
+    });
+}
+
+// gets the first child of a section, removes it, and appends new data
+function changeSection(id, path, callback) {
+    var child = $(id).find(':first-child');
+    var cid = child.attr('id');
+    $("#" + cid).fadeOut('fast', function() {
+        $("#" + cid).remove();
+        appendHTML(id, path);
+        if (callback != undefined) {
+            callback();
+        }
+    });
+}
+
+function storeAndSwitch(path) {
+
+}
