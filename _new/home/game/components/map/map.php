1b8ff213c5d19059c8ad631633daea1c0fb594f7
dweckerly <d.weckerly@gmail.com>
2019-09-13 14:42:45 -0500

    more map component stubbing

--- _new/home/game/components/map/map.php
+++ _new/home/game/components/map/map.php
@@ -4,13 +4,38 @@
 $locId = $gameArr['location'];
 $q = "SELECT * FROM locations WHERE location_id = '$locId'";
 $r = mysqli_query($conn, $q);
-$locationInfo = mysqli_fetch_assoc($r);
+$currentLocation = mysqli_fetch_assoc($r);
 
 // get area info
-$areaId = $locationInfo['area_id'];
+$areaId = $currentLocation['area_id'];
 $q = "SELECT * FROM areas WHERE area_id = '$areaId'";
 $r = mysqli_query($conn, $q);
 $areaInfo = mysqli_fetch_assoc($r);
 
-echo $locationInfo['name'] . " " . $areaInfo['name'];
+// get all locations in area
+$q = "SELECT * FROM locations WHERE area_id = '$areaId'";
+$r = mysqli_query($conn, $q);
+$locations = mysqli_fetch_assoc($r);
+?>
+
+<div>
+    <h2><?php echo $areaInfo['name'];?></h2>
+</div>
+<div>
+    <ul>
+<?php
+foreach($locations as $loc){
+    if($currentLocation['location_id'] == $loc['location_id']) {
+    ?>
+        <li>Current: <?php echo $loc['name'];?></li>
+    <?php
+    } else {
+    ?>
+        <li><?php echo $loc['name'];?></li>
+    <?php
+    }
+}
+?> 
+    </ul>
+</div>
 
