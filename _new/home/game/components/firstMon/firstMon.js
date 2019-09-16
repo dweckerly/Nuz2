92c497b123a83414bd6ea62c97f3099851de2a2f
David <d.weckerly@gmail.com>
2019-09-16 09:38:43 -0500

    fixed header and stylings

--- _new/home/game/components/firstMon/firstMon.js
+++ _new/home/game/components/firstMon/firstMon.js
@@ -1,13 +1,9 @@
 var selected = 0;
 $(document).ready(() => {
-    $("[id^=img]").each(function() {
+    $("[id^=img-]").each(function() {
         this.onclick = function() {
-            $('#img-' + selected).width("-=100");
-            $('#img-' + selected).height("-=100");
             selected = $(this).attr('id').split('-')[1];
-            $("[id^=desc]").hide();
-            $('#img-' + selected).width("+=100");
-            $('#img-' + selected).height("+=100");
+            $("[id^=desc-]").hide();
             $("#desc-" + selected).show();
         }
     });
--- _new/home/game/includes/header.php
+++ _new/home/game/includes/header.php
@@ -4,6 +4,9 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
         <link rel="stylesheet" type="text/css" href="style/fonts.css">
         <link rel="stylesheet" type="text/css" href="style/style.css">
