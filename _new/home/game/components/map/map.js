9ca39c5fbf3f4ed3866c932b456ca6aaa665a909
David <d.weckerly@gmail.com>
2019-09-17 14:05:57 -0500

    more map work

--- _new/home/game/components/map/map.js
+++ _new/home/game/components/map/map.js
@@ -1,6 +1,32 @@
-window.onload = function() {
-    var canvas = document.getElementById("main-canvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("area-map");
-    ctx.drawImage(img, 0, 0, canvas.clientWidth, canvas.height);
-};
+var c = document.getElementById("main-canvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("area-map");
+ctx.drawImage(img, 0, 0, c.width, c.height);
+
+const winOffset = 30;
+
+const cMaxHeight = 540;
+const cMaxWidth = 540;
+
+$(document).ready(function() {
+    setCanvasSize();
+});
+
+$(window).resize(function() {
+    setCanvasSize();
+});
+
+function setCanvasSize() {
+    if ($(window).width() > cMaxWidth) {
+        c.width = cMaxWidth;
+    } else {
+        c.width = $(window).width() - winOffset;
+    }
+
+    if ($(window).height() > cMaxHeight) {
+        c.height = cMaxHeight;
+    }
+    c.height = (c.width * 0.75);
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(img, 0, 0, c.width, c.height);
+}
