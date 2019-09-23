c5b6093f256c9ba52415bf3aba9ddd5a2dbb6560
David <d.weckerly@gmail.com>
2019-09-23 08:35:23 -0500

    stubbing for location actions

--- _new/home/game/components/location/location.trans.php
+++ _new/home/game/components/location/location.trans.php
@@ -9,11 +9,11 @@ if(isset($_POST['id']) && isset($_POST['action'])) {
         $r = mysqli_query($conn, $q);
         $savedLoc = mysqli_fetch_assoc($r);
         $action = $_POST['action'];
+        $newId = $_POST['id'];
         if($action == 'travel') {
             $q = "SELECT * FROM locations WHERE location_id = '$savedLoc'";
             $r = mysqli_query($conn, $q);
             $currentLoc = mysqli_fetch_assoc($r);
-            $newId = $_POST['id'];
             $q = "SELECT * FROM locations WHERE location_id = '$newId'";
             $r = mysqli_query($conn, $q);
             $newLoc = mysqli_fetch_assoc($r);
@@ -21,9 +21,32 @@ if(isset($_POST['id']) && isset($_POST['action'])) {
                 // update game table and send player to new location
             }
         } else if ($action == 'connection') {
+            $q = "SELECT area_id FROM locations WHERE location_id = '$savedLoc'";
+            $r = mysqli_query($conn, $q);
+            $areaId = mysqli_fetch_assoc($r);
+            $q = "SELECT * FROM areas WHERE area_id = '$areaId'";
+            $r = mysqli_query($conn, $q);
+            $currentArea = mysqli_fetch_assoc($r);
+            if($currentArea['north_connection'] == $newId) {
 
+            } elseif($currentArea['east_connection'] == $newId) {
+
+            } elseif($currentArea['south_connection'] == $newId) {
+
+            } elseif($currentArea['west_connection'] == $newId) {
+
+            }
         } else {
-
+            $param = $action . '_id';
+            $q = "SELECT * FROM locations WHERE '$param' = '$id'";
+            $r = mysqli_query($conn, $q);
+            if(mysqli_num_rows($r) > 0) {
+                $table = $action . '_table';
+                $q = "SELECT * FROM '$table' WHERE '$param' = '$id'";
+                $r = mysqli_query($conn, $q);
+                $actionInfo = mysqli_fetch_assoc($r);
+                include("../../components/" . $action . "/" . $action . ".php");
+            }
         }
     }
 }
