608898debf1282faa74b8a06c50cc6129e6a4326
dweckerly <d.weckerly@gmail.com>
2019-09-30 15:07:06 -0500

    style changes

--- _new/home/game/components/wildMon/wildMon.php
+++ _new/home/game/components/wildMon/wildMon.php
@@ -20,21 +20,23 @@ if(isset($_GET['enc_mon'])) {
 <?php
     if($_GET['source'] == "search") {
 ?>
-        <h2 class="center-text">You found a wild <span class="prop-name"><?php echo $monInfo['mon_name'];?></span>!</h2>
-<?php
-    } elseif($_GET['source'] == "travel") {
-?>
-        <h2 class="center-text">A wild <span class="prop-name"><?php echo $monInfo['mon_name'];?> is in your way!</span>!</h2>
-<?php
-    }
-?>
+        <h2 class="center-text no-bold">You found a wild <span class="prop-name bold"><?php echo $monInfo['mon_name'];?></span>!</h2>
     </div>
     <div class="center-text">
         <button onclick="callBattle()">Fight!</button>
-        <!-- Here run button will need to update DB with stored session destination
-        information before returning player to the map -->
         <button onclick="backToMain()">Run!</button>
         <button onclick="callSearch()">Search Again</button>
+<?php
+    } elseif($_GET['source'] == "travel") {
+?>
+        <h2 class="center-text no-bold">A wild <span class="prop-name bold"><?php echo $monInfo['mon_name'];?></span> is in your way!</h2>
+    </div>
+    <div class="center-text">
+        <button onclick="callBattle()">Fight!</button>
+        <button onclick="backToMain()">Run!</button>
+<?php
+    }
+?>
     </div>
     <script src="components/wildMon/wildMon.js"></script>
 </div>
